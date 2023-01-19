db9ee0a44778b36203a71e96345611bbfe551a52
Alasar16 <94788305+Alasar16@users.noreply.github.com>
2023-01-19 14:26:17 +0200

    first commit

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ mongoose.connect(process.env.DB_Connect, ()=>{
     console.log('connected to db..')
 })
 
+app.get('/user',(req,res)=>{
+    res.json({'hello app is running'})
+})
+
 // Import Routes
 const authRoutes = require('./routes/auth')
 const postRoutes= require('./routes/posts')
