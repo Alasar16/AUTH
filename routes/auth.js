887b871d202e1b4300b27de095a73fdae28b9815
Alasar16 <94788305+Alasar16@users.noreply.github.com>
2023-01-19 14:56:04 +0200

    first commit

--- routes/auth.js
+++ routes/auth.js
@@ -38,31 +38,5 @@ router.post('/register' , async(req , res)=>{
 })
 
 
-// // login
-// router.post('/login',async(req,res)=>{
-//      // validation
-//     const{error}=loginValidation(req.body)
-//     if(error) return res.status(400).send(error.details[0].message)
-
-//      //check if the database is exist
-//     const user = await User.findOne({email : req.body.email})
-//     if(!user){
-//         res.status(400).send('email or password is wrong')
-//     }
-//     //password is correct
-//     const invalidpass = await bcrypt.compare(req.body.password,user.password)
-//     if(!invalidpass){
-//         res.status(400).send('Invalid Password')
-//     }
-
-//     // create and asign token
-//     const token=jwt.sign({_id:user._id},process.env.SEKRET_KEY)
-//     res.header( 'auth-token',token).send(token)
-    
-
-
-
-// })
-
 
 module.exports=router
--- validation.js
+++ validation.js
@@ -21,21 +21,9 @@ const registerValidation = (data)=>{
     return schema.validate(data)
 }
 
-const loginValidation = (data)=>{
-    const schema = Joi.object({
-    email : Joi.string().
-    required().
-    min(6).
-    email(),
-    password : Joi.string().
-    required().
-    min(6)
-    } )
-    return schema.validate(data)
-}
 
 module.exports.registerValidation=registerValidation
-module.exports.loginValidation=loginValidation
+
 
 
 
