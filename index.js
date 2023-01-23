550096b6f75171c69a2aca824ad65a9b63f7ad0d
Alasar16 <94788305+Alasar16@users.noreply.github.com>
2023-01-24 00:49:28 +0200

    first commit

--- index.js
+++ index.js
@@ -2,26 +2,58 @@ const express = require('express')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 dotenv.config()
+const User = require('./model/user')
+const {registerValidation}=require('./validation')
 
 const app = express()
-app.get('/user',(req,res)=>{
-    res.json({
-        username:"ibrahim",
-        email:"hema@gmail.com"
+app.use(express.json())
+
+///////////////////////////////////////////////////////
+// Register
+app.post('/register' , async(req , res)=>{
+    // validation
+    const{error}=registerValidation(req.body)
+    if(error) return res.status(400).send(error.details[0].message)
+
+    // //check if the database is exist
+    // const email = await User.findOne({email : req.body.email})
+    // if(email){
+    //     res.status(400).send('email already exist')
+    // }
+    // create a new user
+    const user = new User({
+        firstName : req.body.firstName,
+        lastName : req.body.lastName,
+        email : req.body.email,
+        image:req.body.image
     })
+    
+    try {
+        const savedUser = await user.save()
+        res.send(savedUser)
+    } catch (err) {
+        res.status(200).send(err)
+    }
 })
 
+
+app.get('/users',async(req , res)=>{
+        const allUsers=await User.find()
+        if(allUsers) return res.status(200).send(allUsers)
+    })
+
+////////////////////////////////////////////////////
 //connsect to DB
 mongoose.connect(process.env.DB_Connect, ()=>{
     console.log('connected to db..')
 })
 
-app.use(express.json())
 
-// Import Routes
-const authRoutes = require('./routes/auth')
-// Routes Middleware
-app.use('/api',authRoutes)
+
+// // Import Routes
+// const authRoutes = require('./routes/auth')
+// // Routes Middleware
+// app.use('/api',authRoutes)
 // middleware 
 const port = 3000 || process.env.PORT
 
