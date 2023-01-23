f225cb419c46aff5fe5f455059ffe8a126c30dad
Alasar16 <94788305+Alasar16@users.noreply.github.com>
2023-01-23 20:00:28 +0200

    first commit

--- index.js
+++ index.js
@@ -2,14 +2,45 @@ const express = require('express')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 dotenv.config()
-
 const app = express()
-app.get('/user',(req,res)=>{
-    res.json({
-        username:"ibrahim",
-        email:"hema@gmail.com"
+const User = require('./model/user')
+const {registerValidation}=require('./validation')
+
+
+// Register
+app.post('/register' , async(req , res)=>{
+    // validation
+    const{error}=registerValidation(req.body)
+    if(error) return res.status(400).send(error.details[0].message)
+
+    //check if the database is exist
+    const email = await User.findOne({email : req.body.email})
+    if(email){
+        res.status(400).send('email already exist')
+    }
+    // create a new user
+    const user = new User({
+        firstName : req.body.firstName,
+        lastName : req.body.lastName,
+        email : req.body.email,
+        image:req.body.image
     })
+    
+    try {
+        const savedUser = await user.save()
+        res.send(savedUser)
+    } catch (err) {
+        res.status(200).send(err)
+    }
 })
+
+app.get('/users',async(req , res)=>{
+    const allUsers=await User.find()
+    if(allUsers) res.status(200).json(allUsers)
+})
+
+
+
 app.use(express.json())
 //connsect to DB
 mongoose.connect(process.env.DB_Connect, ()=>{
@@ -19,10 +50,10 @@ mongoose.connect(process.env.DB_Connect, ()=>{
 
 
 // Import Routes
-const authRoutes = require('./routes/auth')
+
 const postRoutes= require('./routes/posts')
 // Routes Middleware
-app.use('/api/user',authRoutes)
+
 app.use('/api/posts',postRoutes)
 // middleware 
 const port = 3000 || process.env.PORT
--- routes/auth.js
+++ routes/auth.js
@@ -1,42 +1,42 @@
-const router =require('express').Router()
-const User = require('../model/user')
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
+// const router =require('express').Router()
+// const User = require('../model/user')
+// const bcrypt = require('bcryptjs')
+// const jwt = require('jsonwebtoken')
 
-const {registerValidation}=require('../validation')
+// const {registerValidation}=require('../validation')
 
-// Register
-router.post('/register' , async(req , res)=>{
-    // validation
-    const{error}=registerValidation(req.body)
-    if(error) return res.status(400).send(error.details[0].message)
+// // Register
+// router.post('/register' , async(req , res)=>{
+//     // validation
+//     const{error}=registerValidation(req.body)
+//     if(error) return res.status(400).send(error.details[0].message)
 
-    //check if the database is exist
-    const email = await User.findOne({email : req.body.email})
-    if(email){
-        res.status(400).send('email already exist')
-    }
-    // create a new user
-    const user = new User({
-        firstName : req.body.firstName,
-        lastName : req.body.lastName,
-        email : req.body.email,
-        image:req.body.image
-    })
+//     //check if the database is exist
+//     const email = await User.findOne({email : req.body.email})
+//     if(email){
+//         res.status(400).send('email already exist')
+//     }
+//     // create a new user
+//     const user = new User({
+//         firstName : req.body.firstName,
+//         lastName : req.body.lastName,
+//         email : req.body.email,
+//         image:req.body.image
+//     })
     
-    try {
-        const savedUser = await user.save()
-        res.send(savedUser)
-    } catch (err) {
-        res.status(200).send(err)
-    }
-})
+//     try {
+//         const savedUser = await user.save()
+//         res.send(savedUser)
+//     } catch (err) {
+//         res.status(200).send(err)
+//     }
+// })
 
-router.get('/users',async(req , res)=>{
-    const allUsers=await User.find()
-    if(allUsers) res.status(200).json(allUsers)
-})
+// router.get('/users',async(req , res)=>{
+//     const allUsers=await User.find()
+//     if(allUsers) res.status(200).json(allUsers)
+// })
 
 
 
-module.exports=router
+// module.exports=router
