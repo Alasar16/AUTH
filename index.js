6f3b1c8dd3b14bec3668f3643ce723f68241a2d9
Alasar16 <94788305+Alasar16@users.noreply.github.com>
2023-02-21 20:18:05 +0200

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
-const mySql = require('mysql')
+const mysql = require('mysql')
+const {registerValidation}=require('./validation')
 const dotenv = require('dotenv')
 dotenv.config()
 
@@ -13,12 +14,56 @@ app.use(express.json())
 //connsect to DB
 
 
+const db = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'user',
+    port:'3306'
+})
 
 
-// Import Routes
-const authRoutes = require('./routes/auth')
-// Routes Middleware
-app.use('/',authRoutes)
+app.get('/user',(req,res)=>{
+    const q = 'SELECT * FROM `registration`'
+    db.query(q,(err,data)=>{
+        if(err) return res.json({msg:'db error'})
+        if(data) return res.json(data)
+    })
+})
+
+app.post('/user',async(req,res)=>{
+    //validation
+
+    const{error}=registerValidation(req.body)
+        if(error) return res.status(400).send(error.details[0].message)
+
+    //new user
+    const{first_name ,last_name , email,password}=req.body
+    const q ='INSERT INTO registration (first_name ,last_name , email,password) VALUES (?,?,?,?)'
+    db.query(q,[first_name ,last_name , email,password],(err,data)=>{
+
+        if(err) return res.json({msg:'db error'})
+
+        if(data){
+            const newQ = 'SELECT * FROM `registration` where id=?'
+    db.query(newQ,[data.insertId],(err,data)=>{
+        if(err) return res.json({msg:'db error'})
+        if(data) return res.json(data)
+    })
+        }
+    })
+})
+
+app.delete('/user/:id',(req,res)=>{
+    const id = req.params.id
+    const q = 'DELETE FROM registration where id=?'
+    db.query(q,[id],(err,data)=>{
+        if(err) return res.json({msg:'db error'})
+        if(data) return res.json(data)
+    })
+
+})
+
 // middleware 
 const port = 3000 || process.env.PORT
 
