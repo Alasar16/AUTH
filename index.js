d9f19ccac999d342d18463417fa5a402b2988f6f
Alasar16 <94788305+Alasar16@users.noreply.github.com>
2023-04-10 16:45:50 +0200

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const mysql = require('mysql')
-const {categoriesValidation}=require('./validation')
 const dotenv = require('dotenv')
 dotenv.config()
 
@@ -27,6 +26,7 @@ const app = express()
 
 app.use(express.json())
 
+// Get categories
 app.get('/categories',(req,res)=>{
     const q = 'SELECT * FROM `categories`'
     db.query(q,(err,data)=>{
@@ -34,14 +34,8 @@ app.get('/categories',(req,res)=>{
         if(data) return res.json(data)
     })
 })
-
+//Post categories
 app.post('/categories',(req,res)=>{
-    //validation
-
-    const{error}=categoriesValidation(req.body)
-        if(error) return res.status(400).send(error.message)
-        
-
     //new user
     const{image ,swappedNumber ,title}=req.body
 
@@ -59,6 +53,43 @@ app.post('/categories',(req,res)=>{
         }
     })
 })
+///////////////////////////////////////////////////
+// Get Comments
+app.get('/Comments',(req,res)=>{
+    const q = 'SELECT * FROM `Comments`'
+    db.query(q,(err,data)=>{
+        if(err) return res.json({msg:'db error'})
+        if(data) return res.json(data)
+    })
+})
+// Post Comments
+app.post('/Comments',(req,res)=>{
+    //new user
+    const{commentedPersonEmail ,Content}=req.body
+
+    const q ='INSERT INTO Comments (commentedPersonEmail ,Content) VALUES (?,?)'
+    db.query(q,[commentedPersonEmail ,Content],(err,data)=>{
+
+        if(err) return res.json({msg:'db error'})
+
+        if(data){
+            const newQ = 'SELECT * FROM `Comments` where PostId=?'
+    db.query(newQ,[data.insertId],(err,data)=>{
+        if(err) return res.json({msg:'db error'})
+        if(data) return res.json(data)
+    })
+        }
+    })
+})
+///////////////////////////////////////////
+app.get('/Posts',(req,res)=>{
+    const q = 'SELECT * FROM `Posts`'
+    db.query(q,(err,data)=>{
+        if(err) return res.json({msg:'db error'})
+        if(data) return res.json(data)
+    })
+})
+
 
 // middleware 
 const port = process.env.PORT || 3000
