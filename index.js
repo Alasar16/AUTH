a7384699f2f9432b4426b6260da737322d859295
Alasar16 <94788305+Alasar16@users.noreply.github.com>
2023-04-12 16:17:39 +0200

    first commit

--- index.js
+++ index.js
@@ -82,11 +82,115 @@ app.post('/Comments',(req,res)=>{
     })
 })
 ///////////////////////////////////////////
+// Get Posts
 app.get('/Posts',(req,res)=>{
     const q = 'SELECT * FROM `Posts`'
     db.query(q,(err,data)=>{
         if(err) return res.json({msg:'db error'})
         if(data) return res.json(data)
+    })
+})
+// Post Posts
+app.post('/Posts',(req,res)=>{
+    //new user
+    const{images ,isKnown,userEmail,videos,content}=req.body
+
+    const q ='INSERT INTO Posts (images ,isKnown,userEmail,videos,content) VALUES (?,?,?,?,?)'
+    db.query(q,[images ,isKnown,userEmail,videos,content],(err,data)=>{
+
+        if(err) return res.json({msg:'db error'})
+
+        if(data){
+            const newQ = 'SELECT * FROM `Posts` where id=?'
+    db.query(newQ,[data.insertId],(err,data)=>{
+        if(err) return res.json({msg:'db error'})
+        if(data) return res.json(data)
+    })
+        }
+    })
+})
+/////////////////////////////////////////
+// Get Donated 
+app.get('/Donated',(req,res)=>{
+    const q = 'SELECT * FROM `Donated`'
+    db.query(q,(err,data)=>{
+        if(err) return res.json({msg:'db error'})
+        if(data) return res.json(data)
+    })
+})
+// Post Donated 
+app.post('/Donated',(req,res)=>{
+    //new user
+    const{donatedPersonEmail ,donattedThing,userEmail,location}=req.body
+
+    const q ='INSERT INTO Donated  (donatedPersonEmail ,donattedThing,userEmail,location) VALUES (?,?,?,?)'
+    db.query(q,[donatedPersonEmail ,donattedThing,userEmail,location],(err,data)=>{
+
+        if(err) return res.json({msg:'db error'})
+
+        if(data){
+            const newQ = 'SELECT * FROM `Donated` where id=?'
+    db.query(newQ,[data.insertId],(err,data)=>{
+        if(err) return res.json({msg:'db error'})
+        if(data) return res.json(data)
+    })
+        }
+    })
+})
+/////////////////////////////////////////
+// Get Swapped 
+app.get('/Swapped',(req,res)=>{
+    const q = 'SELECT * FROM `Swapped`'
+    db.query(q,(err,data)=>{
+        if(err) return res.json({msg:'db error'})
+        if(data) return res.json(data)
+    })
+})
+// Post Swapped 
+app.post('/Swapped',(req,res)=>{
+    //new user
+    const{isKnown ,swappedLocation,Swap2,userEmail2,Swap1,userEmail1}=req.body
+
+    const q ='INSERT INTO Swapped  (isKnown ,swappedLocation,Swap2,userEmail2,Swap1,userEmail1) VALUES (?,?,?,?,?,?)'
+    db.query(q,[isKnown ,swappedLocation,Swap2,userEmail2,Swap1,userEmail1],(err,data)=>{
+
+        if(err) return res.json({msg:'db error'})
+
+        if(data){
+            const newQ = 'SELECT * FROM `Swapped` where id=?'
+    db.query(newQ,[data.insertId],(err,data)=>{
+        if(err) return res.json({msg:'db error'})
+        if(data) return res.json(data)
+    })
+        }
+    })
+})
+/////////////////////////////////////////
+// Get users 
+app.get('/users',(req,res)=>{
+    const q = 'SELECT * FROM `users`'
+    db.query(q,(err,data)=>{
+        if(err) return res.json({msg:'db error'})
+        if(data) return res.json(data)
+    })
+})
+// Post users 
+app.post('/users',(req,res)=>{
+    //new user
+    const{phoneNumber ,address,kind,name,type,email}=req.body
+
+    const q ='INSERT INTO users  (phoneNumber ,address,kind,name,type,email) VALUES (?,?,?,?,?,?)'
+    db.query(q,[phoneNumber ,address,kind,name,type,email],(err,data)=>{
+
+        if(err) return res.json({msg:'db error'})
+
+        if(data){
+            const newQ = 'SELECT * FROM `users` where email=?'
+    db.query(newQ,[data.insertId],(err,data)=>{
+        if(err) return res.json({msg:'db error'})
+        if(data) return res.json(data)
+    })
+        }
     })
 })
 
